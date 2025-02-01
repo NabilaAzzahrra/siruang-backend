dc5c00b00291245e7d31ad50c4a90a36d51b0f30
Lerian Febriana <kanglerian@gmail.com>
2025-02-01 14:33:33 +0700

    nambah socket

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ const allowedOriginSocket = [
   "http://127.0.0.1:8000",
   "http://127.0.0.1:5173",
   "http://localhost:5173",
+  "https://siruang.politekniklp3i-tasikmalaya.ac.id",
 ];
 
 const app = express();
