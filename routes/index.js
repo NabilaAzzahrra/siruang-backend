dec6e291bb35942b0e03773805994715641ba583
Lerian Febriana <kanglerian@gmail.com>
2025-02-01 13:08:26 +0700

    tambah dockerfile

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // return res.json(
-  //   {
-  //     message: 'hhhhhhhhhhhhh'
-  //   }
-  // );
   return res.send('BACKEND SIRUANG ACTIVATED');
 });
 
